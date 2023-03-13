69f2f2aaabe8640e346667aa01ad27003e1c41ea
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-13 10:56:29 +0100

    all

--- resources/views/pages/contact-online.blade.php
+++ resources/views/pages/contact-online.blade.php
@@ -5,7 +5,7 @@
         <section class="appointment-area bg-image ptb-100">
             <div class="container">
                 <div class="appointment-form">
-                    <h4><i class="flaticon-calendar"></i> Make An Appointment</h4>
+                    <h4><i class="flaticon-calendar"></i> Online Termin vereinbaren</h4>
 
                     <form>
                         <div class="form-group">
@@ -17,14 +17,14 @@
                         </div>
 
                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Phone">
+                            <input type="text" class="form-control" placeholder="Telefon">
                         </div>
 
                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Date">
+                            <input type="text" class="form-control" placeholder="Wunschdatum">
                         </div>
 
-                        <button type="submit" class="default-btn">Book Appointment</button>
+                        <button type="submit" class="default-btn">Termin anfragen</button>
                     </form>
                 </div>
             </div>
