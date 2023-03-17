a1e793aca88e7e2aab3b11cf8cd4f73dd36861d1
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 21:27:15 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -5,13 +5,19 @@
             <div class="row py-5 text-start">
                 <div class="col-1"></div>
                 <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="col-7">
-                    <img src="{{$post->image}}">
-                    <h2 class="fw-bold text-primary fs-1">
-                        {!! $post->title !!}
-                    </h2>
-                    <div class="bg-white shadow p-3 rounded rounded-3">
-                        {!! $post->content !!}
+                    <div class="position-relative p-3">
+                        <div class="text-start" style="margin-left: 30px;">
+                            <img src="{{$post->image}}">
+                        </div>
+                        <div class="position-absolute bg-light rounded rounded-3 p-3 shadow" style="margin-top: -110px; margin-right: -16px;">
+                            <h2 class="fw-bold text-primary fs-1">
+                                {!! $post->title !!}
+                            </h2>
+                        </div>
                     </div>
+                    <div class="bg-white shadow p-3 rounded rounded-3 mt-2">
+                        {!! $post->content !!}
+                    </span>
                 </div>
                 <div class="col-4"></div>
             </div>
