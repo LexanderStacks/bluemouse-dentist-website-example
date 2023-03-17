21f1082541767fe793e4ed884e2db5ab240fd111
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 16:13:21 +0100

    next doc

--- resources/views/pages/team/main-doc.blade.php
+++ resources/views/pages/team/main-doc.blade.php
@@ -102,7 +102,7 @@
                 </div>
                 <div class="dentist-details-overview-content">
                     <h3 class="ms-3">Weitere Fortbildungen</h3>
-                    <img src="/images/trainings.jpg">
+                    <img class="py-2" src="/images/trainings.jpg">
                     <ul>
                         <li>
                         Basisausbildung Präventions-Apotheker/In
