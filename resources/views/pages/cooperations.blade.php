50a410883e40f5808cbb2f2163196a71003a9b80
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 20:56:20 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -1,10 +1,10 @@
     <!-- Cooperations -->
         @foreach($posts as $post)
 
-    <div class="container" style="background: orange;">
-                    <div class="row py-5 text-start" style="background: black;">
 
                 @if($loop->index%2 == 0)
+    <div class="container" style="background: orange;">
+                    <div class="row py-5 text-start" style="background: black;">
                         <div key="cooperation-post-{{$loop->index}}-{{$post->slug}}" id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="col-4" style="background: green;">
                             <img src="{{$post->image}}">
                             <h2 class="fw-bold text-primary fs-1">
@@ -14,12 +14,12 @@
                                 {!! $post->content !!}
                             </span>
                         </div>
-                        @endif
             </div>
     </div>
+                        @endif
+                @if($loop->index%2 == 1)
     <div class="container" style="background: orange;">
                     <div class="row py-5 text-start" style="background: black;">
-                @if($loop->index%2 == 1)
                         <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="col-12" style="background: silver;">
                             <img src="{{$post->image}}">
                             <h2 class="fw-bold text-primary fs-1">
@@ -29,8 +29,8 @@
                                 {!! $post->content !!}
                             </span>
                         </div>
-                @endif
             </div>
     </div>
+                @endif
         @endforeach 
     <!-- ./ Cooperations -->
