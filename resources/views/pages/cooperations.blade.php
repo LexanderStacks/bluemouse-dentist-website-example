2b87eda3b83e4170194f4f4ae23264cf7de97765
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 19:47:11 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -7,7 +7,7 @@
                     <h2 class="fw-bold text-primary fs-1">
                         {!! $post->title !!}
                     </h2>
-                    <div style="padding-left: -40px">
+                    <div style="margin-left: -40px">
                         {!! $post->content !!}
                     </div>
                 </div>
