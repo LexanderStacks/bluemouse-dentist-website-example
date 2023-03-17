c91998071ad0d66db93b11f8a1e01a29dc1ebcd5
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 20:49:22 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -1,24 +1,10 @@
     <!-- Cooperations -->
     <div class="container" style="background: orange;">
-        @foreach($posts as $post)
-                @if($loop->index%2 == 0)
-                <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}">
-                    <div class="row py-5 text-start" style="background: blue;">
-                        <div class="col-12" style="background: green;">
-                            <img src="{{$post->image}}">
-                            <h2 class="fw-bold text-primary fs-1">
-                                {!! $post->title !!}
-                            </h2>
-                            <span style="background: magenta;">
-                                {!! $post->content !!}
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                @else
                 <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}">
                     <div class="row py-5 text-start" style="background: black;">
-                        <div class="col-12" style="background: silver;">
+        @foreach($posts as $post)
+                @if($loop->index%2 == 0)
+                        <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="col-12" style="background: green;">
                             <img src="{{$post->image}}">
                             <h2 class="fw-bold text-primary fs-1">
                                 {!! $post->title !!}
@@ -27,8 +13,16 @@
                                 {!! $post->content !!}
                             </span>
                         </div>
-                    </div>
-                </div>
+                @else
+                        <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="col-12" style="background: silver;">
+                            <img src="{{$post->image}}">
+                            <h2 class="fw-bold text-primary fs-1">
+                                {!! $post->title !!}
+                            </h2>
+                            <span style="background: magenta;">
+                                {!! $post->content !!}
+                            </span>
+                        </div>
                 @endif
             </div>
         @endforeach 
