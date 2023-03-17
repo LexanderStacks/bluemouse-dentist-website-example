0755d1627bc63e718bc63a789ccf083ae11804d0
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 19:45:27 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -1,17 +1,17 @@
     <!-- Cooperations -->
     <div class="container">
-        <div class="row py-5 text-start">
-            <div class="col-12">
-                @foreach($posts as $post)
-                <img src="{{$post->image}}">
-                <h2 class="fw-bold text-primary fs-1">
-                    {!! $post->title !!}
-                </h2>
-                <p>
-                    {!! $post->content !!}
-                </p>
-                @endforeach 
+        @foreach($posts as $post)
+            <div class="row py-5 text-start">
+                <div class="col-4 @if($loop->index%2 == 1) me-auto @else ms-auto @endif">
+                    <img src="{{$post->image}}">
+                    <h2 class="fw-bold text-primary fs-1">
+                        {!! $post->title !!}
+                    </h2>
+                    <div style="padding-left: -40px">
+                        {!! $post->content !!}
+                    </div>
+                </div>
             </div>
-        </div>
+        @endforeach 
     </div>
     <!-- ./ Cooperations -->
