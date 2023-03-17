9b77361ca9b83f2ce29c8d1b85b0c9ace76bdb82
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 19:55:20 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -1,16 +1,31 @@
     <!-- Cooperations -->
     <div class="container-fluid" style="background: orange;">
         @foreach($posts as $post)
-            <div class="row py-5 text-start" style="background: blue;">
-                <div class="col-7 @if($loop->index%2 == 1) me-auto @else ms-auto @endif" style="background: green;">
-                    <img src="{{$post->image}}">
-                    <h2 class="fw-bold text-primary fs-1">
-                        {!! $post->title !!}
-                    </h2>
-                    <div style="margin-left: -40px" style="background: magenta;">
-                        {!! $post->content !!}
+                @if($loop->index%2 == 1)
+                <div class="row py-5 text-start" style="background: blue;">
+                    <div class="col-7 ms-auto" style="background: green;">
+                        <img src="{{$post->image}}">
+                        <h2 class="fw-bold text-primary fs-1">
+                            {!! $post->title !!}
+                        </h2>
+                        <div style="margin-left: -40px; bakckground: magenta; width: calc(100% + 40px);" >
+                            {!! $post->content !!}
+                        </div>
                     </div>
                 </div>
+                @else
+                <div class="row py-5 text-start" style="background: blue;">
+                    <div class="col-7 me-auto" style="background: green;">
+                        <img src="{{$post->image}}">
+                        <h2 class="fw-bold text-primary fs-1">
+                            {!! $post->title !!}
+                        </h2>
+                        <div style="margin-left: -40px; bakckground: magenta; width: calc(100% + 40px);">
+                            {!! $post->content !!}
+                        </div>
+                    </div>
+                </div>
+                @endif
             </div>
         @endforeach 
     </div>
