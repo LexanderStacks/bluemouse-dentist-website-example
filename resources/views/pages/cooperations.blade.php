5dc80b81520858955bb0ead7efb578f2a1c2584d
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 21:01:45 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -1,36 +1,38 @@
-    <!-- Cooperations -->
-        @foreach($posts as $post)
-
-
-                @if($loop->index%2 == 0)
-    <div class="container" style="background: orange;">
-                    <div class="row py-5 text-start" style="background: black;">
-                        <div key="cooperation-post-{{$loop->index}}-{{$post->slug}}" id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="col-4" style="background: green;">
-                            <img src="{{$post->image}}">
-                            <h2 class="fw-bold text-primary fs-1">
-                                {!! $post->title !!}
-                            </h2>
-                            <span style="background: magenta;">
-                                {!! $post->content !!}
-                            </span>
-                        </div>
+<!-- Cooperations -->
+@foreach($posts as $post)
+    @if($loop->index%2 == 0)
+        <div class="container">
+            <div class="row py-5 text-start">
+                <div class="col-1"></div>
+                <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="col-7">
+                    <img src="{{$post->image}}">
+                    <h2 class="fw-bold text-primary fs-1">
+                        {!! $post->title !!}
+                    </h2>
+                    <div class="bg-white shadow">
+                        {!! $post->content !!}
+                    </div>
+                </div>
+                <div class="col-4"></div>
             </div>
-    </div>
-                        @endif
-                @if($loop->index%2 == 1)
-    <div class="container" style="background: orange;">
-                    <div class="row py-5 text-start" style="background: black;">
-                        <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="col-12" style="background: silver;">
-                            <img src="{{$post->image}}">
-                            <h2 class="fw-bold text-primary fs-1">
-                                {!! $post->title !!}
-                            </h2>
-                            <span style="background: magenta;">
-                                {!! $post->content !!}
-                            </span>
-                        </div>
+        </div>
+    @endif
+    @if($loop->index%2 == 1)
+        <div class="container">
+            <div class="row py-5 text-start">
+                <div class="col-4"></div>
+                <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="col-7">
+                    <img src="{{$post->image}}">
+                    <h2 class="fw-bold text-primary fs-1">
+                        {!! $post->title !!}
+                    </h2>
+                    <div class="bg-white shadow">
+                        {!! $post->content !!}
+                    </span>
+                </div>
+                <div class="col-1"></div>
             </div>
-    </div>
-                @endif
-        @endforeach 
-    <!-- ./ Cooperations -->
+        </div>
+    @endif
+@endforeach 
+<!-- ./ Cooperations -->
