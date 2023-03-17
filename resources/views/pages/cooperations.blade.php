83141632536b8ee38f1eef577286cdbbb78fb9f8
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 20:11:04 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -2,19 +2,22 @@
     <div class="container-fluid" style="background: orange;">
         @foreach($posts as $post)
                 @if($loop->index%2 == 1)
-                <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="row py-5 text-start" style="background: blue;">
-                    <div class="col-7 ms-auto" style="background: green;">
-                        <img src="{{$post->image}}">
-                        <h2 class="fw-bold text-primary fs-1">
-                            {!! $post->title !!}
-                        </h2>
-                        <div style="margin-left: -40px; bakckground: magenta; width: calc(100% + 40px);" >
-                            {!! $post->content !!}
+                <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}">
+                    <div class="row py-5 text-start" style="background: blue;">
+                        <div class="col-7 ms-auto" style="background: green;">
+                            <img src="{{$post->image}}">
+                            <h2 class="fw-bold text-primary fs-1">
+                                {!! $post->title !!}
+                            </h2>
+                            <div style="margin-left: -40px; bakckground: magenta; width: calc(100% + 40px);" >
+                                {!! $post->content !!}
+                            </div>
                         </div>
                     </div>
                 </div>
                 @else
-                <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="row py-5 text-start" style="background: blue;">
+                <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}">
+                    <div class="row py-5 text-start" style="background: blue;">
                     <div class="col-7 me-auto" style="background: silver;">
                         <img src="{{$post->image}}">
                         <h2 class="fw-bold text-primary fs-1">
@@ -24,6 +27,7 @@
                             {!! $post->content !!}
                         </div>
                     </div>
+                </div>
                 </div>
                 @endif
             </div>
