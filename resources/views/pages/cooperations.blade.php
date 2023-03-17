152b58bc63fbed802a0eb682e4157583afdaa5ad
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 19:47:32 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -1,8 +1,8 @@
     <!-- Cooperations -->
-    <div class="container">
+    <div class="container-fluid">
         @foreach($posts as $post)
             <div class="row py-5 text-start">
-                <div class="col-4 @if($loop->index%2 == 1) me-auto @else ms-auto @endif">
+                <div class="col-7 @if($loop->index%2 == 1) me-auto @else ms-auto @endif">
                     <img src="{{$post->image}}">
                     <h2 class="fw-bold text-primary fs-1">
                         {!! $post->title !!}
