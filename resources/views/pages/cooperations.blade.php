43ca28ddb3caaa3e8c9b7ebfb5890fe7b0552cf4
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 20:09:01 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid" style="background: orange;">
         @foreach($posts as $post)
                 @if($loop->index%2 == 1)
-                <div class="row py-5 text-start" style="background: blue;">
+                <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="row py-5 text-start" style="background: blue;">
                     <div class="col-7 ms-auto" style="background: green;">
                         <img src="{{$post->image}}">
                         <h2 class="fw-bold text-primary fs-1">
@@ -14,7 +14,7 @@
                     </div>
                 </div>
                 @else
-                <div class="row py-5 text-start" style="background: blue;">
+                <div id="cooperation-post-{{$loop->index}}-{{$post->slug}}" class="row py-5 text-start" style="background: blue;">
                     <div class="col-7 me-auto" style="background: silver;">
                         <img src="{{$post->image}}">
                         <h2 class="fw-bold text-primary fs-1">
