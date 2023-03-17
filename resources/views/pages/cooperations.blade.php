83bcaf0e3fa867356998ad3cebaaffc6135f753a
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 18:26:44 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -1,15 +1,17 @@
-    <!-- Test -->
-    <div class="row py-5 text-start">
-        <div class="col-12">
-            @foreach($posts as $post)
-            <img src="{{$post->image}}">
-            <h2 class="fw-bold text-primary fs-1">
-                {!! $post->title !!}
-            </h2>
-            <p>
-                {!! $post->content !!}
-            </p>
-            @endforeach 
+    <!-- Cooperations -->
+    <div class="container">
+        <div class="row py-5 text-start">
+            <div class="col-12">
+                @foreach($posts as $post)
+                <img src="{{$post->image}}">
+                <h2 class="fw-bold text-primary fs-1">
+                    {!! $post->title !!}
+                </h2>
+                <p>
+                    {!! $post->content !!}
+                </p>
+                @endforeach 
+            </div>
         </div>
     </div>
-    <!-- ./ Test -->
+    <!-- ./ Cooperations -->
