cac2eda259c24b18060db0da4a94ea26242a9767
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-17 19:51:56 +0100

    cooperations

--- resources/views/pages/cooperations.blade.php
+++ resources/views/pages/cooperations.blade.php
@@ -1,13 +1,13 @@
     <!-- Cooperations -->
-    <div class="container-fluid">
+    <div class="container-fluid" style="background: orange;">
         @foreach($posts as $post)
-            <div class="row py-5 text-start">
-                <div class="col-7 @if($loop->index%2 == 1) me-auto @else ms-auto @endif">
+            <div class="row py-5 text-start" style="background: blue;">
+                <div class="col-7 @if($loop->index%2 == 1) me-auto @else ms-auto @endif" style="background: green;">
                     <img src="{{$post->image}}">
                     <h2 class="fw-bold text-primary fs-1">
                         {!! $post->title !!}
                     </h2>
-                    <div style="margin-left: -40px">
+                    <div style="margin-left: -40px" style="background: magenta;">
                         {!! $post->content !!}
                     </div>
                 </div>
