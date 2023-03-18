7a194249b34313e5c286570ae1afb40e68abb3f9
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-18 11:54:48 +0100

    hygiene

--- resources/views/pages/hygiene.blade.php
+++ resources/views/pages/hygiene.blade.php
@@ -6,8 +6,8 @@
         @foreach($posts as $post)
           <div class="accordion-item">
             <h2 class="accordion-header" id="flush-headingOne-{{$loop->index}}">
-              <button class="accordion-button collapsed accordion-collapse" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne-{{$loop->index}}" aria-expanded="false" aria-controls="flush-collapseOne">
-                {!! $post->title !!}
+              <button class="accordion-button collapsed fs-2 text-black" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne-{{$loop->index}}" aria-expanded="false" aria-controls="flush-collapseOne">
+                {{$post->title }}
               </button>
             </h2>
             <div id="flush-collapseOne-{{$loop->index}}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
