d4f110e27fefd0fab72762527808e130e9c68bae
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-18 18:09:13 +0100

    ltc

--- resources/views/pages/covid-therapy.blade.php
+++ resources/views/pages/covid-therapy.blade.php
@@ -1,8 +1,14 @@
 <div class="container-fluid g-0 px-2">
     <div class="row py-5 text-center text-xl-start">
     	<div class="col-12">
-			{!! $post->title !!}
-			{!! $post->content !!}
+    		<h1>
+				{{ $post->title }}
+			</h1>
+    	</div>
+    	<div class="col-12">
+			<div>
+				{!! $post->content !!}
+			</div>
     	</div>
     </div>
 </div>
