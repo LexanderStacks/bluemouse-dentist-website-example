d5f8e83bd73eda43911dd7cbf48104031f36969c
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-18 12:19:26 +0100

    hygiene

--- resources/views/pages/hygiene.blade.php
+++ resources/views/pages/hygiene.blade.php
@@ -6,11 +6,11 @@
         @foreach($posts as $post)
           <div class="accordion-item">
             <h2 class="accordion-header" id="flush-headingOne-{{$loop->index}}">
-              <button class="accordion-button collapsed fs-3 text-primary" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne-{{$loop->index}}" aria-expanded="@if($loop->index == 1) true @else false @endif" aria-controls="flush-collapseOne-{{$loop->index}}">
+              <button class="accordion-button collapsed fs-3 text-primary" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne-{{$loop->index}}" aria-expanded="@if($loop->index == 0) true @else false @endif" aria-controls="flush-collapseOne-{{$loop->index}}">
                 {{ $post->title }}
               </button>
             </h2>
-            <div id="flush-collapseOne-{{$loop->index}}" class="accordion-collapse collapse @if($loop->index == 1) show @endif" aria-labelledby="flush-headingOne-{{$loop->index}}" data-bs-parent="#accordionFlushExample">
+            <div id="flush-collapseOne-{{$loop->index}}" class="accordion-collapse collapse @if($loop->index == 0) show @endif" aria-labelledby="flush-headingOne-{{$loop->index}}" data-bs-parent="#accordionFlushExample">
               <div class="accordion-body">
                 {!! $post->content !!}
               </div>
