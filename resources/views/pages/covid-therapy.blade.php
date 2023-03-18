b88e83cc890bfccf7f97543ad28fd508a810cf93
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-18 18:22:21 +0100

    ltc

--- resources/views/pages/covid-therapy.blade.php
+++ resources/views/pages/covid-therapy.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid g-0 px-5">
-    <div class="row py-5 text-center text-xl-start px-2">
+    <div class="row py-5 px-5">
     	<div class="col-12">
     		<h2>
 				{{ $post->title }}
