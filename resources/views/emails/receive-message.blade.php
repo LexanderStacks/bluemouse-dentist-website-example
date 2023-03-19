93fbd6bce096c4ddfbb81e1b0a2a112a5cf501bf
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-19 19:55:38 +0100

    email

---
+++ resources/views/emails/receive-message.blade.php
@@ -0,0 +1,39 @@
+<body style="padding:0px;margin:0px;font-family: sans-serif; background: green;">
+
+
+    <p style="color: white;">
+        Über das Kontaktformular wurde folgende Mitteilung abgeschickt:
+    </p>
+    <h3 style="color:  white; font-weight: bold;">
+        {{$message}}
+    </h3>
+    <br>
+    <br>
+    
+       ABSENDER
+    <br>
+    <span style="color: white;">
+        Name: 
+        <b>
+            <span style="font-weight: bold;">
+            {{$name}}
+            </span>
+        </b>
+        <br>
+        <br>
+        E-mail: 
+        <b>
+            <span style="font-weight: bold;">
+            {{$email}}
+            </span>
+        </b>
+        Company: 
+        <b>
+            <span style="font-weight: bold;">
+            {{$company}}
+            </span>
+        </b>
+    </span>
+
+
+</body>
