3f99d59042a277dbb97485a0c9ea6ca961de42b7
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-19 07:44:14 +0100

    website

--- resources/views/pages/inc/service-contact.blade.php
+++ resources/views/pages/inc/service-contact.blade.php
@@ -1,8 +1,8 @@
 
 	<div class="row px-3 px-xl-5 pb-3">
-	    <div class="col-12 text-end pt-2 pb-5">
-	        <h4>Beratung und Termine</h4>
-	        <p>
+	    <div class="col-12 text-start py-5">
+	        <h3>Beratung und Termine</h3>
+	        <p style="text-black">
 	        	Melden Sie sich unter der Rufnummer <a href="tel:02119849880"> <i class="bi bi-telephone-forward"></i> +49 211 984 988 0 </a> oder <a href="/contact"> online </a>, um einen Termin in unserer ganzheitlich ausgerichteten Zahnarztpraxis in Düsseldorf (Stadtmitte) zu vereinbaren – Wir freuen uns darauf, Ihnen durch ganzheitliche Zahnimplantate zu mehr Lebensqualität zu verhelfen.
 	        </p>            
 	    </div>
