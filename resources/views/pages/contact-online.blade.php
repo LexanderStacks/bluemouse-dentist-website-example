8a264f8353de02438f29c80672dd55409f50ede6
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-19 23:37:44 +0100

    flatpickr locale de

--- resources/views/pages/contact-online.blade.php
+++ resources/views/pages/contact-online.blade.php
@@ -1,32 +1,39 @@
+<section class="appointment-area bg-image ptb-100">
+    <div class="container">
+        <div class="appointment-form">
+            <h4><i class="flaticon-calendar"></i> Online Termin vereinbaren</h4>
 
-
-
-        <!-- Start Appointment Area -->
-        <section class="appointment-area bg-image ptb-100">
-            <div class="container">
-                <div class="appointment-form">
-                    <h4><i class="flaticon-calendar"></i> Online Termin vereinbaren</h4>
-
-                    <form>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Name">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Email">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Telefon">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Wunschdatum">
-                        </div>
-
-                        <button type="submit" class="default-btn">Termin anfragen</button>
-                    </form>
+            <form>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Name">
                 </div>
-            </div>
-        </section>
-        <!-- End Appointment Area -->
+
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Email">
+                </div>
+
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Telefon">
+                </div>
+
+                <div class="form-group">
+                    <input id="wishDateOnline" type="text" class="form-control" placeholder="Ihr Wunschdatum">
+                </div>
+
+                <button type="submit" class="default-btn">Termin anfragen</button>
+            </form>
+        </div>
+    </div>
+</section>
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        flatpickr("#wishDateOnline", {
+            enableTime: false,
+            dateFormat: "Y-m-d",
+            weekNumbers: true,
+            altInput: true,
+            altFormat: "j. F Y",  
+            locale: "de",
+        });
+    });
+</script>
