7089e3c9a1f8ac843f453b0e5205608b007f83eb
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-19 10:22:11 +0100

    website

--- resources/views/pages/inc/detox/comparison.blade.php
+++ resources/views/pages/inc/detox/comparison.blade.php
@@ -18,7 +18,7 @@
             <div class="col-md-8 pt-4">
               <div class="card-body">
                 <h5 class="card-title text-primary">Gesunder Zahn</h5>
-                <p class="card-text">Die Entgiftungswoche ermöglicht Ihnen innerhalb von nur fünf Tagen Ihre Gebisssanierung auf höchstem biologisch-zahnmedizinischen Niveau. Befreien Sie sich von Störfeldern, NICOs, unverträglichen Titanimplantaten, chronisch infizierten Wurzelfüllungen und korrodierenden Zahnmetallen in Ihrer Mundhöhle und gönnen Sie sich Entspannung und Erholung im hauseigenen Medical SPA!</p>
+                <p class="card-text text-black">Die Entgiftungswoche ermöglicht Ihnen innerhalb von nur fünf Tagen Ihre Gebisssanierung auf höchstem biologisch-zahnmedizinischen Niveau. Befreien Sie sich von Störfeldern, NICOs, unverträglichen Titanimplantaten, chronisch infizierten Wurzelfüllungen und korrodierenden Zahnmetallen in Ihrer Mundhöhle und gönnen Sie sich Entspannung und Erholung im hauseigenen Medical SPA!</p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
             </div>
@@ -36,7 +36,7 @@
             <div class="col-md-8 pt-4">
               <div class="card-body">
                 <h5 class="card-title text-danger">Toter Zahn</h5>
-                <p class="card-text">
+                <p class="card-text text-black">
                   Die Entgiftungswoche ist ein speziell auf die Patienten von FirstBioDent (Dr. Lammers & Kollegen) zugeschnittenes Therapiekonzept, das seit fast 30 Jahren besteht. Das Behandlungsschema orientiert sich an der Vorgehensweise der Umweltmedizin und der amerikanischen Funktionellen Medizin, die Spezialisten, wie Prof. William Rea/Dallas und Prof. Jeffrey Bland/Gig Harbour Dr. Lammers vor über 25 Jahren beigebrachten.
                 </p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
