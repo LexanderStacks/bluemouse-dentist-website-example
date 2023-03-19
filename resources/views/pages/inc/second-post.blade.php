afe26a0e75f9bf8da17fca8d6d5846a8d119e217
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-19 09:08:00 +0100

    website

---
+++ resources/views/pages/inc/second-post.blade.php
@@ -0,0 +1,14 @@
+    <!-- wp content -->
+    <div class="row py-5 px-3 px-xl-5">
+    	<div class="col-12">
+    		<h4 class="text-primary fs-1">
+				{{ $secondPost->title }}
+			</h4>
+    	</div>
+    	<div class="col-12 mt-3">
+			<div>
+				{!! $secondPost->content !!}
+			</div>
+    	</div>
+    </div>
+    <!-- ./wp content -->
