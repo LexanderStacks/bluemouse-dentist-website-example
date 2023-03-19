b73d062cbf72735e10aa470e53fd3148b9ee24e3
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-19 11:30:27 +0100

    website

--- resources/views/pages/inc/detox/days.blade.php
+++ resources/views/pages/inc/detox/days.blade.php
@@ -6,7 +6,20 @@
     </div>
     <div class="col-12 mt-3">
       <div>
-        <div class="card-group">
+        <div class="d-none d-xl-block card-group">
+          @foreach($elements as $element)
+          <div class="card">
+            <img src="{{$element->image}}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h3 class="card-title fs-2 text-primary">{{$element->title}}</h3>
+              <p class="card-text">
+                {!! $element->content !!}
+              </p>
+            </div>
+          </div>
+          @endforeach
+        </div>
+        <div class="d-block d-xl-none">
           @foreach($elements as $element)
           <div class="card">
             <img src="{{$element->image}}" class="card-img-top" alt="...">
