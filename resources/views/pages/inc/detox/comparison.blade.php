f33382db8c0fdb67a1f1b0f02465a215319f97da
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-19 10:21:47 +0100

    website

--- resources/views/pages/inc/detox/comparison.blade.php
+++ resources/views/pages/inc/detox/comparison.blade.php
@@ -8,9 +8,9 @@
     </div>
 
 
-      <div class="col-xl-6 mt-3 h-100">
+      <div class="col-xl-6 mt-3">
 
-        <div class="card mb-3 border-none no-border shadow w-100 h-100" style="border: none;">
+        <div class="card mb-3 border-none no-border shadow w-100" style="border: none;">
           <div class="row g-0 align-items-center">
             <div class="col-md-4 pt-4 pb-4">
               <img src="/images/gesunder-zahn.png" class="img-fluid rounded-start" alt="...">
@@ -26,9 +26,9 @@
         </div>
 
       </div>
-      <div class="col-xl-6 mt-3 h-100">
+      <div class="col-xl-6 mt-3">
 
-        <div class="card mb-3 shadow w-100 h-100" style="border: none;">
+        <div class="card mb-3 shadow w-100" style="border: none;">
           <div class="row g-0 align-items-center">
             <div class="col-md-4 pt-4 pb-4">
               <img src="/images/toter-zahn.png" class="img-fluid rounded-start" alt="...">
@@ -37,7 +37,7 @@
               <div class="card-body">
                 <h5 class="card-title text-danger">Toter Zahn</h5>
                 <p class="card-text">
-                  Die Entgiftungswoche ist ein speziell auf die Patienten von FirstBioDent (Dr. Lammers & Kollegen) zugeschnittenes Therapiekonzept, das seit fast 30 Jahren besteht. Das Behandlungsschema orientiert sich an der Vorgehensweise der Umweltmedizin und der amerikanischen Funktionellen Medizin, die Spezialisten, wie Prof. William Rea/Dallas und Prof. Jeffrey Bland/Gig Harbour Herrn Dr. Lammers vor über 25 Jahren beigebracht haben.
+                  Die Entgiftungswoche ist ein speziell auf die Patienten von FirstBioDent (Dr. Lammers & Kollegen) zugeschnittenes Therapiekonzept, das seit fast 30 Jahren besteht. Das Behandlungsschema orientiert sich an der Vorgehensweise der Umweltmedizin und der amerikanischen Funktionellen Medizin, die Spezialisten, wie Prof. William Rea/Dallas und Prof. Jeffrey Bland/Gig Harbour Dr. Lammers vor über 25 Jahren beigebrachten.
                 </p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
