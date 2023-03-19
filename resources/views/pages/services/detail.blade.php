e3b5e2e43458477bf9ca599badeeb2b3e70c926f
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-19 13:26:39 +0100

    website

--- resources/views/pages/services/detail.blade.php
+++ resources/views/pages/services/detail.blade.php
@@ -1,11 +1,13 @@
 <div class="container-fluid g-0 px-3 px-xl-5">
 
     <!-- wp content -->
-    <div class="row py-5 px-3 px-xl-5">
-        <div class="col-12">
+    <div class="row py-5 px-3 px-xl-5 align-items-center">
+        <div class="col-6">
             <div class="position-relative">
                 <img style="max-height: 340px; width: auto;" src="{{$post->image}}" class="img-fluid rounded rounded-3 border border-5 border-white shadow text-center">
-                <div class="position-absolute bg-white px-3 py-2 shadow text-start" style="display: inline-block; margin-top: 220px; margin-left: 120px;">
+            </div>
+            <div class="col-6 position-relative text-end h-100">
+                <div class="row h-100 bg-white px-3 py-2 shadow align-items-center" style="display: inline-block;">
                     <h2 class="fs-1 text-primary">{{ $post->title }}</h2>
                 </div>
             </div>
