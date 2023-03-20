de785e7db7381c4a9cdb97e341473f431b08a068
Alex <alexanderkucharski@MacBook-Pro-von-Alexander.local>
2023-03-20 09:58:31 +0100

    all

--- resources/views/pages/inc/detox/days.blade.php
+++ resources/views/pages/inc/detox/days.blade.php
@@ -1,3 +1,13 @@
+<style>
+  .scale{
+    transition: .2s;
+    cursor: pointer;
+  }
+  .scale:hover{
+    transform: scale(1.1);
+  }
+</style>
+
   <div class="row pb-3 pt-2 px-3 px-xl-5">
     <div class="col-12">
       <h2 class="text-primary fs-1">
@@ -8,7 +18,7 @@
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 row-cols-xxl-5 g-1">
         @foreach($elements as $element)
         <div id="element-{{$loop->index}}-{{$element->slug}}" class="col">
-          <div class="card h-100 shadow" style="border: none;">
+          <div class="card h-100 shadow scale" style="border: none;">
             <img src="{{$element->image}}" class="card-img-top" alt="...">
             <div class="card-body">
                <h3 class="card-title fs-2 text-primary">{{$element->title}}</h3>
