15e42805c88ea35b9900abdb382082be5254bd0b
Alex <alexanderkucharski@MBPvonAlexander.fritz.box>
2023-03-29 17:07:24 +0200

    no import '../sass/template/dark.scss'

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,5 @@
 import './bootstrap';
 import '../sass/app.scss';
-import '../sass/template/dark.scss';
 import '../sass/template/responsive.scss';
 import '../sass/template/style.scss';
 import * as bootstrap from 'bootstrap';
